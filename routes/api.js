905f17f4ae72b873854a352e9664d47452a6bce0
Fabian <fabianffc@gmail.com>
2020-12-27 12:40:40 -0600

    heroku deploy

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 var db = require("../db/db.json")
 var router = require("express").Router()
+const { v4: uuidv4 } = require('uuid');
+var fs = require("fs")
 
 //Api route GET
 router.get("/api/notes", function(req, res){
@@ -7,14 +9,34 @@ router.get("/api/notes", function(req, res){
 })
 //Api route POST
 router.post("/api/notes", function(req, res){
+  req.body.id = uuidv4()
     db.push(req.body)
-    res.json(db)
+    console.log(db);
+    fs.writeFile("./db/db.json",JSON.stringify(db),function(error){
+      if (error){
+        console.log(error);
+    
+      }
+      res.json(db)
+    })
 })
 
 //Api route DELETE
 router.delete("/api/notes/:id",function(req, res){
-  db.pop(req.body)
-  res.json(db)
+    console.log(req.params.id);
+    var id = req.params.id
+    for(var i = 0; i<db.length; i++){
+         if (db[i].id === id){
+           db.splice(i,1)
+         }
+    }
+    fs.writeFile("./db/db.json",JSON.stringify(db),function(error){
+      if (error){
+        console.log(error);
+      }
+      res.json(db)
+
+    })
 });
 
 module.exports = router
